7d3da582b1c0c1f062cc46e940c3fdad0f8c2414
DRL\heych <D@rr3nH3lm>
2015-05-21 11:21:40 +0100

    IdentityServer up and running

---
+++ src/IdentityServer.Spike/App_Start/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Owin;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Thinktecture.IdentityServer.Core.Models;
+
+namespace IdentityServer.Spike
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.Map("/identity", idsrvApp =>
+			{
+				idsrvApp.UseIdentityServer(new IdentityServerOptions
+				{
+					SiteName = "Embedded IdentityServer",
+					SigningCertificate = LoadCertificate(),
+
+					Factory = InMemoryFactory.Create(
+						users: Users.Get(),
+						clients: Clients.Get(),
+						scopes: StandardScopes.All)
+				});
+			});
+		}
+
+		X509Certificate2 LoadCertificate()
+		{
+			return new X509Certificate2(
+				string.Format(@"{0}\bin\idsrv3test.pfx", AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
+		}
+	}
+}
